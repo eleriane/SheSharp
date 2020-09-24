0dccc680897ca2362a4f41e285a9aa6e6a671f9f
Eleriane <nanebnu_1@hotmail.com>
2020-09-24 15:05:43 -0300

    Commit inicial

---
+++ DotNetConf/Program.cs
@@ -0,0 +1,42 @@
+﻿using DotNetConf.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetConf
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //  restcookbook.com 
+
+            //  martinfowler.com/articles/richardsonMaturityModel.html
+
+
+            var host = BuildWebHost(args);
+            SeedDb(host);
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+
+        private static void SeedDb(IWebHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<DotNetConfSeeder>();
+                seeder.SeedDatabaseAsync().Wait();
+            }
+        }
+    }
+}
---
+++ DotNetConf/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using DotNetConf.Data;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+namespace DotNetConf
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DotNetConfContext>();
+            services.AddTransient<DotNetConfSeeder>();
+
+            services.AddMvc().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddApiVersioning(cfg =>
+            {
+                cfg.DefaultApiVersion = new ApiVersion(1, 1);
+                cfg.AssumeDefaultVersionWhenUnspecified = true;
+                cfg.ReportApiVersions = true;
+
+                //cfg.ApiVersionReader = new QueryStringApiVersionReader("v");
+
+                cfg.ApiVersionReader = ApiVersionReader.Combine(
+                  new HeaderApiVersionReader("X-Version"),
+                new QueryStringApiVersionReader("v"));
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllers();
+            });
+        }
+    }
+}
